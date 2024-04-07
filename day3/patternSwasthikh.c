9c3a4b969f60051af6a65b15409ec3265316b402
HelloShiv <swagatswaroopparida@gmail.com>
2024-04-07 23:35:48 +0530

    completed swasthik symbol

--- day3/patternSwasthikh.c
+++ day3/patternSwasthikh.c
@@ -4,12 +4,22 @@ int main(){
     for (i = 1; i <= n; i++){
         
         for (j = 1; j <= n;j++){
-            if( i == mid || j == mid)
+            if( i == mid  || j == mid )
                 printf("* ");
+            else if( j == 1 && i < mid)
+                printf("* ");
+            else if( j == n && i > mid)
+                printf("* ");
+            else if( i == n && j < mid)
+                printf("* ");
+            else if( i == 1 && j > mid)
+                printf("* ");
+            else
+                printf("  ");
             
         }
         printf("\n");
     }
 
-    // Incomplete 
+    // completed 
 }
